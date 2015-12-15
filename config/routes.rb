077604fcf70efb627bf83f7b5eb793bb207ea5bf
Nikos Mouchtaris <mouchtaris@gmail.com>
2015-12-15 22:04:44 +0000

    Name user routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 Rails.application.routes.draw do
 
-  get '/register' => 'users#new'
-  post '/register' => 'users#register'
+  get '/register' => 'users#new', as: :user
+  post '/register' => 'users#register', as: :new_user
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
