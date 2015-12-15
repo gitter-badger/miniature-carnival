4d30d2e3443230b11e30ebdb13dcbf8726b69441
Nikos Mouchtaris <mouchtaris@gmail.com>
2015-12-15 21:14:11 +0000

    Fix migration error: email not null, passwd null

--- db/migrate/20151215203204_create_users.rb
+++ db/migrate/20151215203204_create_users.rb
@@ -1,8 +1,8 @@
 class CreateUsers < ActiveRecord::Migration
   def change
     create_table :users do |t|
-      t.string :email, limit: 60
-      t.string :password, limit: 40, null: false
+      t.string :email, limit: 60, null: false
+      t.string :password, limit: 40
       t.string :type, limit: 40, null: false
 
       t.timestamps null: false
