25d94d87fadaf01bd5e811dda84405b87203c78d
Nikos Mouchtaris <mouchtaris@gmail.com>
2015-12-15 20:48:29 +0000

    Add the user model

---
+++ db/migrate/20151215203204_create_users.rb
@@ -0,0 +1,12 @@
+class CreateUsers < ActiveRecord::Migration
+  def change
+    create_table :users do |t|
+      t.string :email, limit: 60
+      t.string :password, limit: 40, null: false
+      t.string :type, limit: 40, null: false
+
+      t.timestamps null: false
+    end
+    add_index :users, :email, unique: true
+  end
+end
