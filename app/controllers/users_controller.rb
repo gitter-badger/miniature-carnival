3fa0df16df18d6f158554ea526bfd5848f71f13b
Nikos Mouchtaris <mouchtaris@gmail.com>
2015-12-15 22:16:11 +0000

    Complete user registration

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,9 +1,24 @@
 class UsersController < ApplicationController
 
   def new
+    @user = User.new
   end
 
   def register
+    @user = User.new(user_params)
+    if @user.save
+      render :success
+    else
+      @errors = @user.errors.full_messages.map(&:to_s)
+      render :error
+    end
+  end
+
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password, :type)
   end
 
 end
