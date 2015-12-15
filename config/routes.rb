13ad5013237c174ff0309901abfac43ecc5ddb8d
Nikos Mouchtaris <mouchtaris@gmail.com>
2015-12-15 22:19:00 +0000

    Add user listing

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,7 @@ Rails.application.routes.draw do
 
   get '/register' => 'users#new', as: :user
   post '/register' => 'users#register', as: :new_user
+  get '/users' => 'users#index', as: :users
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
